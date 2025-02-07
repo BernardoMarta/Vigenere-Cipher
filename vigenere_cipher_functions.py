aba8a8bdeb04712ec6b29de3f02681ae6dd80bb6
BernardoMarta <71236109+BernardoMarta@users.noreply.github.com>
2025-02-07 14:48:39 +0000

    vigenere_cipher_functions.py

--- vigenere_cipher_functions.py
+++ vigenere_cipher_functions.py
@@ -1,5 +1,11 @@
 ALPHABET='abcdefghijklmnopqrstuvwxyz'
 
+
+chave_cifra = input('Insira a chave:')
+texto = input('Insira o texto a encriptar')
+
+
+#first version
 def vigenere(chave,txt):
     txt_cifrado=''
     chave
@@ -12,7 +18,9 @@ def rodar(chave2,letra):
     nova_letra = chr((n+ord(chave2)-ord('a'))%26+ord('a'))
     return nova_letra
 
-####
+vigenere(chave_cifra,texto)
+
+#second version
 def vigenere2(chave,txt):
     txt_cifrado=''
     chave = chave*1000
@@ -24,3 +32,5 @@ def rodar2(chave2,letra):
     n=ALPHABET.index(letra)
     nova_letra = ALPHABET[(n+ALPHABET.index(chave2))%26]
     return nova_letra
+
+vigenere2(chave_cifra,texto)
